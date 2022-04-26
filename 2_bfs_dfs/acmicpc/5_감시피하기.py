7b47dacf2807d59f54703943c297c77f6927a1f9
philhoonoh <vlfgns5@naver.com>
2022-04-26 11:12:27 +0900

    adding bfs dfs problem 5_

--- 2_bfs_dfs/acmicpc/5_감시피하기.py
+++ 2_bfs_dfs/acmicpc/5_감시피하기.py
@@ -1,45 +1,75 @@
 # https://github.com/ndb796/python-for-coding-test
 
+from itertools import combinations
+from copy import deepcopy
+
 n = int(input())
 graph = []
-for i
+x_cord = []
+for i in range(n):
+    graph.append(list(map(str, input().split())))
+    for j, value in enumerate(graph[-1]):
+        if value == 'X':
+            x_cord.append((i, j))
 
-def dfs(n, value, numbers, plus, minus, mul, div):
-    if n == 0:
-        answer.append(value)
+wall_cand_lst = list(combinations(x_cord, 3))
 
-    if plus > 0:
-        value_ = value + numbers[0]
-        numbers_ = numbers[1:]
-        plus_ = plus - 1
-        n_ = n - 1
-        dfs(n_, value_, numbers_, plus_, minus, mul, div)
-    if minus > 0:
-        value_ = value - numbers[0]
-        numbers_ = numbers[1:]
-        minus_ = minus - 1
-        n_ = n - 1
-        dfs(n_, value_, numbers_, plus, minus_, mul, div)
-    if mul > 0:
-        value_ = value * numbers[0]
-        numbers_ = numbers[1:]
-        mul_ = mul - 1
-        n_ = n - 1
-        dfs(n_, value_, numbers_, plus, minus, mul_, div)
-    if div > 0:
-        if value < 0 and numbers[0] > 0:
-            value_ = abs(value) // numbers[0] * -1
-        else:
-            value_ = value // numbers[0]
-        numbers_ = numbers[1:]
-        div_ = div - 1
-        n_ = n - 1
-        dfs(n_, value_, numbers_, plus, minus, mul, div_)
+def dfs(i, j, n, temp_graph, k):
 
-inital_value = numbers[0]
-numbers = numbers[1:]
-n -= 1
-answer = []
-dfs(n, inital_value, numbers, plus, minus, mul, div)
-print(max(answer))
-print(min(answer))
+    if k == 0:
+        next_i = i
+        next_j = j + 1
+    if k == 1:
+        next_i = i
+        next_j = j - 1
+    if k == 2:
+        next_i = i - 1
+        next_j = j
+    if k == 3:
+        next_i = i + 1
+        next_j = j
+
+    if not (-1 < next_i < n and -1 < next_j < n):
+        return True
+    elif -1 < next_i < n and -1 < next_j < n:
+        if temp_graph[next_i][next_j] == 'S':
+            return False
+        elif temp_graph[next_i][next_j] == 'O':
+            return True
+        elif temp_graph[next_i][next_j] == 'T':
+            return True
+        elif temp_graph[next_i][next_j] == 'X':
+            return dfs(next_i, next_j, n, temp_graph, k)
+
+
+def search(n, graph_temp):
+    for i in range(n):
+        for j in range(n):
+            if graph_temp[i][j] == 'T':
+                for k in range(4):
+                    # False equal to Fail (Teacher Found Student Case)
+                    if not dfs(i, j, n, graph_temp, k):
+                        return False
+    return True
+
+answer_flag = False
+for walls in wall_cand_lst:
+    graph_temp = deepcopy(graph)
+
+    for x, y in walls:
+        graph_temp[x][y] = 'O'
+
+    if search(n, graph_temp):
+        answer_flag = True
+
+    if answer_flag:
+        break
+
+if answer_flag:
+    print("YES")
+else:
+    print("NO")
+
+
+
+

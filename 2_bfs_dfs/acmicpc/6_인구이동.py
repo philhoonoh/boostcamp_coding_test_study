3c71cdf2f59b906c9f8566ec4a65870aa9b42d61
philhoonoh <vlfgns5@naver.com>
2022-04-27 10:34:44 +0900

    adding problem to bfs_dfs

--- 2_bfs_dfs/acmicpc/6_인구이동.py
+++ 2_bfs_dfs/acmicpc/6_인구이동.py
@@ -1,75 +1,62 @@
 # https://www.acmicpc.net/problem/16234
 
-from itertools import combinations
-from copy import deepcopy
+from collections import deque
 
-n = int(input())
+# 동 북 서 남
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+
+n, l, r = map(int, input().split())
 graph = []
-x_cord = []
 for i in range(n):
-    graph.append(list(map(str, input().split())))
-    for j, value in enumerate(graph[-1]):
-        if value == 'X':
-            x_cord.append((i, j))
-
-wall_cand_lst = list(combinations(x_cord, 3))
-
-def dfs(i, j, n, temp_graph, k):
-
-    if k == 0:
-        next_i = i
-        next_j = j + 1
-    if k == 1:
-        next_i = i
-        next_j = j - 1
-    if k == 2:
-        next_i = i - 1
-        next_j = j
-    if k == 3:
-        next_i = i + 1
-        next_j = j
-
-    if not (-1 < next_i < n and -1 < next_j < n):
-        return True
-    elif -1 < next_i < n and -1 < next_j < n:
-        if temp_graph[next_i][next_j] == 'S':
-            return False
-        elif temp_graph[next_i][next_j] == 'O':
-            return True
-        elif temp_graph[next_i][next_j] == 'T':
-            return True
-        elif temp_graph[next_i][next_j] == 'X':
-            return dfs(next_i, next_j, n, temp_graph, k)
+    graph.append(list(map(int, input().split())))
 
 
-def search(n, graph_temp):
+def bfs(i, j, visited, graph, l, r, n):
+    queue = deque()
+    queue.append((i, j, graph[i][j]))
+    cord_lst = []
+    total = 0
+    cnt = 0
+
+    while queue:
+        x, y, value = queue.popleft()
+        cord_lst.append([x, y])
+        cnt += 1
+        total += value
+        for delta_x, delta_y in zip(dx, dy):
+            next_x, next_y = x + delta_x, y + delta_y
+            if -1 < next_x < n and -1 < next_y < n and (next_x, next_y) not in visited \
+                and l <= abs(graph[x][y] - graph[next_x][next_y]) <= r:
+                queue.append((next_x, next_y, graph[next_x][next_y]))
+                visited.add((next_x, next_y))
+    return cord_lst, total, cnt
+
+
+day_cnt = 0
+while True:
+    border_lst = []
+    average_lst = []
+    visited = set()
+
+    flag_no_change = True
     for i in range(n):
         for j in range(n):
-            if graph_temp[i][j] == 'T':
-                for k in range(4):
-                    # False equal to Fail (Teacher Found Student Case)
-                    if not dfs(i, j, n, graph_temp, k):
-                        return False
-    return True
+            if (i, j) not in visited:
+                visited.add((i, j))
+                border_open_lst, total, cnt = bfs(i, j, visited, graph, l, r, n)
+                value = total // cnt
+                if cnt != 1:
+                    flag_no_change = False
+                border_lst.append(border_open_lst)
+                average_lst.append(value)
 
-answer_flag = False
-for walls in wall_cand_lst:
-    graph_temp = deepcopy(graph)
-
-    for x, y in walls:
-        graph_temp[x][y] = 'O'
-
-    if search(n, graph_temp):
-        answer_flag = True
-
-    if answer_flag:
+    if flag_no_change:
+        print(day_cnt)
         break
-
-if answer_flag:
-    print("YES")
-else:
-    print("NO")
-
-
-
+    else:
+        day_cnt += 1
+        for border, value in zip(border_lst, average_lst):
+            for i, j in border:
+                graph[i][j] = value
 
